bdcc0d1f988cc31eed6c5f9d3def117dfbd2b9f1
Мария Клепикова <masha.k2001@gmail.com>
2024-05-28 19:57:25 +0300

    [add] BusinessValidation, StructureValidation

---
+++ TicTacToe_2/Validation/BusinessValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_2
+{
+    public class BusinessValidation
+    {
+        public static bool CellStatusValidation(CellStatus[,] _playingField, Point playerMove)
+        {
+            if (_playingField[playerMove.valueAxisY, playerMove.valueAxisX] == CellStatus.Empty)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TicTacToe_2/Validation/StructureValidation.cs
@@ -0,0 +1,48 @@
+﻿namespace TicTacToe_2
+{
+    public class StructureValidation  
+    {
+        public static bool UserInputValidation(string userInput) 
+        {
+            userInput = userInput.ToUpper();
+
+            return
+                 CheckNumberOfSymbolsInInput(userInput) &&
+                 CheckValueOfLetter(userInput) &&
+                 CheckValueOfNumber(userInput);
+        }
+
+        private static bool CheckNumberOfSymbolsInInput(string userInput)
+        {
+            int numberOfSimpols = userInput.Length;
+
+            if (numberOfSimpols == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckValueOfLetter(string userInput)
+        {
+            char[] correctYValues = { 'A', 'B', 'C' };
+
+            if (correctYValues.Contains(userInput[0]))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckValueOfNumber(string userInput)
+        {
+            char[] correctXValues = {'1', '2', '3'};
+
+            if (correctXValues.Contains(userInput[1]))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
